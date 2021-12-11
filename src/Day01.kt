cb9f3518845c6af60b0a7b8b66176d9d910b962e
Simonas <simsas18@gmail.com>
2021-12-11 19:51:27 +0200

    Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        var increaseCount = 0
+        for ((index, depth) in depths.withIndex()) {
+            if (index > 0 && depth > depths[index - 1]) {
+                increaseCount++
+            }
+        }
+        return increaseCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        var increaseCount = 0
+        for ((index, _) in depths.withIndex()) {
+            if (index > 2 && depths[index] > depths[index - 3]) {
+                increaseCount++
+            }
+        }
+        return increaseCount
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
